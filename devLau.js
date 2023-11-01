13cb34f01a1707ef97270a1579413675a666aeb1
Lautaro <LautaroFer456@gmail.com>
2023-11-01 17:00:34 -0300

    PrimeraConexionExitosa

--- devLau.js
+++ devLau.js
@@ -1,49 +1,37 @@
-const listaDiscosEl = document.querySelectorAll(".discoElement .img")
-
-
-
+const {VITE_API_KEY: key} = import.meta.env
 
 async function getData(){
-    
-    const url = 'https://spotify23.p.rapidapi.com/albums/?ids=3IBcauSj5M2A6lTeffJzdv&limit=5';
-    const options = {
+    const url = 'https://spotify23.p.rapidapi.com/search/?q=one3E&type=multi&offset=0&limit=1&numberOfTopResults=1';
+const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '40f07326b6msh39606ec9e90292ap16b27ajsn224f8e74083b',
+		'X-RapidAPI-Key': `${key}`,
 		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-
-    const {albums} = result
-    console.log(albums[0])
-
-
-
-    return (albums[0])
-	// return result.albums[0]
-} catch (error) {
-	console.error(error);
+fetch(url, options)
+.then((res) => res.json())
+.then(data => {
+    console.log(data.albums.items[0].data)
+    crearDisco(data.albums.items[0].data)
+})
+.catch(erros =>{console.log(erros)})
 
 }
 
+function crearDisco(data1){
+    const discoEl = document.querySelector('#discoPrueba')
+
+    if(!data1){
+        return
+    }
+    const img = document.createElement("img")
+    img.src = data1.coverArt.sources[0].url
+    discoEl.appendChild(img)
 }
 
+getData()
 
-function cargarDisco(){
-    const data = getData()
 
-    alert(data)
-
-    // listaDiscosEl.forEach(disco => {
-    //     const img = document.createElement("img")
-    //     img.alt = data.id
-    //     disco.appendChild(img)
-    // });
-}
-
-cargarDisco()
 
