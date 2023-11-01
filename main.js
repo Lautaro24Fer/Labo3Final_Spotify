fa3af6c362ef0b79eecca0cbe0253cac45e1dc77
Fachifachi <gist140920@gmail.com>
2023-11-01 15:06:07 -0300

    Cambié la forma para la búsqueda
    Para probar, hay que actualizar el token y poner el ID y el IdSecret que proporciona la API

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-
-
-
-var G_client_id = 'CLIENT_ID';
-var G_client_secret = 'CLIENT_SECRET';
+var G_client_id = 'bc16d65feba34c608d8450e9d764d834';
+var G_client_secret = 'e3192148ff3e4499b1d11b79371c43df';
 
 var authOptions = {
   method: 'POST',
   headers: {
-    'Authorization': 'Basic ' + btoa (G_client_id + ':' + G_client_secret),
+    'Authorization': 'Basic ' + btoa(G_client_id + ':' + G_client_secret),
     'Content-Type': 'application/x-www-form-urlencoded'
   },
   body: 'grant_type=client_credentials'
@@ -17,38 +14,39 @@ fetch('https://accounts.spotify.com/api/token', authOptions)
   .then(response => response.json())
   .then(data => {
     var token = data.access_token;
-    console.log(token);
+    console.log('Token de acceso:', token);
+
+    // Agregar un controlador de eventos al botón de búsqueda
+    document.getElementById('buscarButton').addEventListener('click', function() {
+      buscarArtistasOAlbumes(token);
+    });
   })
   .catch(error => console.error('Error:', error));
 
-
-// Agrega un controlador de eventos al botón de búsqueda
-document.getElementById('buscarButton').addEventListener('click', function() {
-  G_buscarArtistasOAlbumes();
-});
-
 // Función para buscar artistas o álbumes en la API de Spotify
-function G_buscarArtistasOAlbumes() {
+function buscarArtistasOAlbumes(token) {
   const barraBusqueda = document.getElementById('barraBusqueda');
-  const busqueda = barraBusqueda.value; // Obtén el término de búsqueda del input
+  const busqueda = barraBusqueda.value.trim(); // Obtener el término de búsqueda del input y eliminar espacios en blanco
 
-  const token = 'BQDeDnLULTSQpk433YJ2xjH0DhVhjIQO1DWbjuQ3U5vLT3XSbVGy9gsfv-WEJgulpmSEEV90sIK-rM_0lc4KKrfqhD1OPPHd1iYHv3H4NdBovsmXLjc'; // Reemplaza con tu propio token de acceso
-
-  // Realiza la solicitud a la API de Spotify utilizando el término de búsqueda
-  fetch(`https://api.spotify.com/v1/search?q=${busqueda}&type=artist`, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${token}`, // Establece el token de acceso en los encabezados
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      // Maneja la respuesta de la búsqueda aquí
-      console.log('Resultados de la búsqueda:', data);
-      // Implementa cómo deseas mostrar los resultados en tu interfaz
+  if (busqueda) {
+    // Realizar la solicitud a la API de Spotify solo si hay un término de búsqueda válido
+    fetch(`https://api.spotify.com/v1/search?q=${busqueda}&type=artist`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}` // Establecer el token de acceso en los encabezados
+      }
     })
-    .catch(error => {
-      console.error('Error al realizar la búsqueda:', error);
-      // Maneja los errores de la solicitud
-    });
+      .then(response => response.json())
+      .then(data => {
+        // Manejar la respuesta de la búsqueda aquí
+        console.log('Resultados de la búsqueda:', data);
+        // Implementar cómo mostrar los resultados en tu interfaz
+      })
+      .catch(error => {
+        console.error('Error al realizar la búsqueda:', error);
+        // Manejar los errores de la solicitud
+      });
+  } else {
+    console.error('No se proporcionó un término de búsqueda válido');
+  }
 }
