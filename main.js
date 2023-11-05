3297d60b29b1450db3738f60efb2774be3dfc4d9
Fachifachi <gist140920@gmail.com>
2023-11-05 20:26:26 -0300

    La búsqueda tira resultados en la web

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var G_client_id = '';
 var G_client_secret = '';
 var token = ''; // Variable global para el token
+const searchContainer = document.getElementById('searchContainer'); // Asegúrate de tener un contenedor con id "searchContainer"
 
 // Renovación para el uso de los tokens y su renovación automáticamente
 function obtenerNuevoToken() {
@@ -41,7 +42,11 @@ function buscarDatos() {
       .then(response => response.json())
       .then(data => {
         console.log('Resultados de la búsqueda:', data);
-        // Mostrar los resultados en la interfaz 
+        
+        // Limpiar el contenedor antes de mostrar los nuevos resultados
+        searchContainer.innerHTML = '';
+        
+        mostrarResultados(data, tipoSeleccionado); // Llama a la función para mostrar los resultados en la interfaz
       })
       .catch(error => {
         console.error('Error al realizar la búsqueda:', error);
@@ -50,9 +55,74 @@ function buscarDatos() {
     console.error('No se proporcionó un término de búsqueda válido');
   }
 }
+function mostrarResultados(data, tipoSeleccionado) {
+  const resultados = data[tipoSeleccionado + 's'].items.slice(0, 7); // Limitar los resultados a 7 elementos
+
+  resultados.forEach(resultado => {
+    let src, name;
+
+    if (tipoSeleccionado === 'track') {
+      // Si es una canción, la imagen puede no estar en el campo "images", usa otro método
+      if (resultado.album && resultado.album.images && resultado.album.images.length > 0) {
+        src = resultado.album.images[0].url;
+      } else {
+        src = '../assets/default_image.png';
+      }
+    } else {
+      if (resultado.images && resultado.images.length > 0) {
+        src = resultado.images[0].url;
+      } else {
+        src = '../assets/default_image.png';
+      }
+    }
+
+    if (resultado.name) {
+      name = resultado.name;
+    } else {
+      name = 'Nombre no disponible';
+    }
+
+    generateDiv(
+      resultado.href, 
+      src, 
+      name, 
+      tipoSeleccionado
+    );
+  });
+}
+
+function generateDiv(href, src, name, type, imgClass = "", btn = "") {
+  if (src === null) {
+    src = "../assets/default_user.png";
+  }
+
+  const div = document.createElement('div');
+  div.classList.add('results-container');
+
+  const img = document.createElement('img');
+  img.src = src;
+  img.alt = `logo-${name}`;
+  img.className = `result-img w-24 justify-center items-center ${imgClass}`;
+  img.draggable = 'false';
+
+  const nameHeader = document.createElement('h5');
+  nameHeader.textContent = name;
+  nameHeader.classList.add('result-name');
+
+  const typeHeader = document.createElement('h6');
+  typeHeader.textContent = type;
+  typeHeader.classList.add('result-type');
+  typeHeader.classList.add('text-gray-400');
+
+  div.appendChild(img);
+  div.appendChild(nameHeader);
+  div.appendChild(typeHeader);
+
+  searchContainer.appendChild(div);
+}
 
 // Obtener un nuevo token al inicio
 obtenerNuevoToken();
 
-// Renovacion del token, ni idea si se puede aumentar o dismunuir, pero funciona piola
+// Renovación del token, no está clara su manipulación
 setInterval(obtenerNuevoToken, 50 * 60 * 1000); // Renovar cada 50 minutos
