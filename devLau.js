3a0969eb3ebbd925ae69fad34242a36ef4ccf7b0
Lautaro <LautaroFer456@gmail.com>
2023-11-05 22:12:13 -0300

    FrontendDetalles2

--- devLau.js
+++ devLau.js
@@ -202,6 +202,8 @@ function mostrarFeed() {
 
 mostrarFeed()
 
+
+//animacion del boton que redirecciona a spotify
 const LF_albumsArray = document.querySelectorAll(".discoElementSpotify")
 
 LF_albumsArray.forEach(album => {
@@ -218,3 +220,81 @@ LF_albumsArray.forEach(album => {
         btn.style.opacity = "0"
     })
 });
+
+//hover en navbar
+
+const LF_navBtnEl = document.querySelectorAll(".navButton")
+
+LF_navBtnEl.forEach(btn => {
+    btn.addEventListener("mouseenter", () => {
+        let svg = btn.querySelector("svg")
+        let text = btn.querySelector("h6")
+
+        svg.style.transition = "all .2s ease-in"
+        text.style.transition = "all .2s ease-in"
+        svg.style.fill = "rgb(238, 238, 238)"
+        text.style.color = "rgb(238, 238, 238)"
+    });
+    btn.addEventListener("mouseleave", () => {
+        let svg = btn.querySelector("svg")
+        let text = btn.querySelector("h6")
+
+        svg.style.transition = "all .2s ease-in"
+        text.style.transition = "all .2s ease-in"
+        svg.style.fill = "rgb(91, 91, 91)"
+        text.style.color = "rgb(91, 91, 91)"
+    });
+});
+
+//hover en los botones de redes
+const LF_mediaButtonsEl = document.querySelectorAll("article.media button")
+
+LF_mediaButtonsEl.forEach(btn => {
+
+    btn.addEventListener("mouseenter", () => {
+        let svg = btn.querySelector("svg")
+
+        if (btn.id === "igbtn") {
+            btn.style.background = "linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)"
+        }
+        if (btn.id === "xbtn") {
+            btn.style.background = "black"
+
+        }
+        if (btn.id === "fbbtn") {
+            btn.style.background = "rgb(127, 127, 255)"
+        }
+        btn.style.height = "3.8em"
+        btn.style.width = "3.8em"
+        svg.style.height = "1.9em"
+    })
+    btn.addEventListener("mouseleave", () => {
+        let svg = btn.querySelector("svg")
+
+        btn.style.height = "3.5em"
+        svg.style.height = "1.7em"
+        btn.style.width = "3.5em"
+        btn.style.background = "rgb(45, 45, 45)"
+    })
+    cont++
+});
+
+//hover en la biblioteca
+
+const biblioEl = document.querySelector("#idBiblio")
+
+biblioEl.addEventListener("mouseenter", () => {
+    let svg = biblioEl.querySelector("svg")
+    let text = biblioEl.querySelector("h6")
+
+    svg.style.fill = "rgb(238, 238, 238)"
+    text.style.color = "rgb(238, 238, 238)"
+})
+
+biblioEl.addEventListener("mouseleave", () => {
+    let svg = biblioEl.querySelector("svg")
+    let text = biblioEl.querySelector("h6")
+
+    svg.style.fill = "rgb(91, 91, 91)"
+    text.style.color = "rgb(91, 91, 91)"
+})
